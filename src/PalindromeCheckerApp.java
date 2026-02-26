f097256d9c52a0be13e5f4ef6455de4db2eedfc2
rgopika132 <rgopika132@gmail.com>
2026-02-26 15:53:22 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -14,25 +14,45 @@
  */
 
   public class PalindromeCheckerApp {
-//Use Case 1: Application Entry &Welcome Message
-    // Application constants
+  /**
+   * UseCase2PalindromeCheckerApp
+   *
+   * Objective:
+   * Check whether a hardcoded string is a palindrome
+   * and print the result.
+   *
+   * Concepts Used:
+   * - Class
+   * - Main Method
+   * - Static Keyword
+   * - String & String Literal
+   * - if-else Conditional Statement
+   * - Console Output
+   */
 
-    static final String VERSION = "1.0";
 
-    /**
-     * Main method - Entry point of the Java application
-     * JVM invokes this method automatically.
-     */
+
     public static void main(String[] args) {
 
-      // Display Welcome Message
-      System.out.println("=====================================");
-      System.out.println(" Welcome to Palindrome Checker" );
+      // Hardcoded string (String Literal)
+      String word = "MALAYALAM";
 
-      System.out.println("=====================================");
-      System.out.println("Application Name : Palindrome Checker");
-      System.out.println("Application Version : " + VERSION);
-      System.out.println("Application started successfully.");
-      System.out.println("Ready for palindrome validation...");
+      // Variable to store reversed string
+      String reversed = "";
+
+      // Reverse the string using loop
+      for (int i = word.length() - 1; i >= 0; i--) {
+        reversed = reversed + word.charAt(i);
+      }
+
+      // Check palindrome using if-else
+      if (word.equals(reversed)) {
+        System.out.println("The word \"" + word + "\" is a Palindrome.");
+      } else {
+        System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+      }
+
+      System.out.println("Program executed successfully.");
     }
   }
+
